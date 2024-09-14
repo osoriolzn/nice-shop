480b4af52b37e707abbd2d5954fca600feb2ff29
Yohan D Osorio l <yooslzn1487@gmail.com>
2024-09-14 16:29:31 -0500

    Add basic navbar

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,36 @@
+import { NavLink } from 'react-router-dom'
+
+function NavBar() {
+  return (
+    <nav className='flex justify-between items-center bg-gray-100 fixed z-10 top-0 w-full py-5 px-8 text-sm'>
+      <ul className='flex items-center gap-3'>
+        <li>
+          <NavLink to='/'>
+            <span className='text-lg font-bold'>Nice Shop</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/'>Hombres</NavLink>
+        </li>
+        <li>
+          <NavLink to='/'>Mujeres</NavLink>
+        </li>
+        <li>
+          <NavLink to='/'>Joyería</NavLink>
+        </li>
+        <li>
+          <NavLink to='/'>Electrónicos</NavLink>
+        </li>
+      </ul>
+
+      <ul className='flex items-center gap-3'>
+        <li>
+          <NavLink to='/'>Ordenes</NavLink>
+        </li>
+        <img src='src/assets/shoppingCartIcon.svg' alt='Shopping Cart svg' />
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
