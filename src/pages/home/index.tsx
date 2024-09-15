ddb33ec3a05c147410d93e6bccdee8e80c1decde
Yohan D Osorio l <yooslzn1487@gmail.com>
2024-09-14 20:53:34 -0500

    Show product card on home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,37 @@
+import Card from '../../components/card'
+import Layout from '../../components/layout'
+import itemsToRender from '../../mocks/products.json'
+
 function Home() {
+  const renderView = () => {
+    if (itemsToRender.length > 0) {
+      return itemsToRender.map(item => (
+        <Card key={item.id} data={item} />
+      ))
+    }
+  }
+
   return (
-    <h1>Welcome to My Home Page</h1>
+    <Layout>
+      <div className='flex items-center justify-center relative w-80 mb-2'>
+        <h1 className='font-medium text-xl'>Descubre Tu Estilo</h1>
+      </div>
+
+      <input
+        id='search'
+        name='search'
+        // value={search}
+        // onChange={(event) => setSearch(event.target.value)}
+        className='rounded-lg border border-black w-80 p-2 mb-4'
+        type='text'
+        placeholder='White Gold, SanDisk...'
+      />
+
+      <div className='grid grid-cols-3 gap-3 w-full max-w-screen-lg'>
+        {renderView()}
+      </div>
+
+    </Layout>
   )
 }
 
