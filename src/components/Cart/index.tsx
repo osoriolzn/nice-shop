9ad5e6d598ca0344c9fa971af2329f5758d09bb7
Yohan D Osorio l <yooslzn1487@gmail.com>
2024-09-21 15:49:59 -0500

    Refactor cart component

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -2,10 +2,19 @@ import { useContext } from 'react'
 import { ShopContext } from '../../context'
 
 function ShoppingCart() {
-  const { shoppingCarts } = useContext(ShopContext);
+  const { shoppingCarts, openCheckoutMenu } = useContext(ShopContext)
+
+  const openCheckout = () => {
+    openCheckoutMenu()
+  }
+
   return (
-    <div className='relative flex gap-0.5 items-center'>
-      <img src='src/assets/shoppingCartIcon.svg' alt='Shopping Cart svg' />
+    <div className='relative flex gap-0.5 items-center' onClick={openCheckout}>
+      <img
+        className='cursor-pointer'
+        src='src/assets/shoppingCartIcon.svg'
+        alt='Shopping Cart svg'
+      />
       <span className='absolute bottom-3.5 left-3.5 flex justify-center items-center rounded-full bg-blue-500 w-4 h-4 font-semibold text-xs text-white'>
         {shoppingCarts.length}
       </span>
