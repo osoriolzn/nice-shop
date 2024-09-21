ff6c7358ce97f7414498e447162161d2ae802819
Yohan D Osorio l <yooslzn1487@gmail.com>
2024-09-21 15:58:33 -0500

    Update context

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,10 +1,14 @@
 import { ReactNode, createContext, useEffect, useState } from 'react'
 import responseData from '../mocks/products.json'
-import { ProductType, ShopContextType } from './types'
+import { ProductType, ShopContextType } from '../types/types'
 
 const defaultContext: ShopContextType = {
+  isCheckoutMenuOpen: false,
   products: [],
   shoppingCarts: [],
+  closeCheckoutMenu: () => {},
+  openCheckoutMenu: () => {},
+  setIsCheckoutMenuOpen: () => {},
   setProducts: () => {},
   setShoppingCarts: () => {},
 }
@@ -12,18 +16,26 @@ const defaultContext: ShopContextType = {
 export const ShopContext = createContext(defaultContext)
 
 export function ShopProvider({ children }: {children: ReactNode}) {
+  const [isCheckoutMenuOpen, setIsCheckoutMenuOpen] = useState<boolean>(false)
   const [products, setProducts] = useState<ProductType[]>([])
   const [shoppingCarts, setShoppingCarts] = useState<ProductType[]>([])
   
   useEffect(() => {
     setProducts(responseData)
   }, [])
+  
+  const openCheckoutMenu = () => setIsCheckoutMenuOpen(true)
+  const closeCheckoutMenu = () => setIsCheckoutMenuOpen(false)
 
   return (
     <ShopContext.Provider
       value={{
+        isCheckoutMenuOpen,
         products,
         shoppingCarts,
+        closeCheckoutMenu,
+        openCheckoutMenu,
+        setIsCheckoutMenuOpen,
         setProducts,
         setShoppingCarts
       }}
