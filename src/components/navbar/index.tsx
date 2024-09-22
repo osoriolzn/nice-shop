443f668b0ee80d561a1aba406e8f1f4f5152b230
Yohan D Osorio l <yooslzn1487@gmail.com>
2024-09-21 19:08:03 -0500

    Set categories to filter

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,26 +1,29 @@
+import { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import { ShopContext } from '../../context'
 import ShoppingCart from '../cart'
 
 function NavBar() {
+  const { setSearchCategory } = useContext(ShopContext)
   return (
     <nav className='flex justify-between items-center bg-gray-100 fixed z-10 top-0 w-full py-5 px-8 text-sm'>
       <ul className='flex items-center gap-3'>
-        <li>
+        <li onClick={() => setSearchCategory('')}>
           <NavLink to='/'>
             <span className='text-lg font-bold'>Nice Shop</span>
           </NavLink>
         </li>
-        <li>
-          <NavLink to='/'>Hombres</NavLink>
+        <li onClick={() => setSearchCategory("men's clothing")}>
+          <NavLink to='/men'>Hombres</NavLink>
         </li>
-        <li>
-          <NavLink to='/'>Mujeres</NavLink>
+        <li onClick={() => setSearchCategory("women's clothing")}>
+          <NavLink to='/women'>Mujeres</NavLink>
         </li>
-        <li>
-          <NavLink to='/'>Joyería</NavLink>
+        <li onClick={() => setSearchCategory('jewelery')}>
+          <NavLink to='/jewelry'>Joyería</NavLink>
         </li>
-        <li>
-          <NavLink to='/'>Electrónicos</NavLink>
+        <li onClick={() => setSearchCategory('electronics')}>
+          <NavLink to='/electronics'>Electrónicos</NavLink>
         </li>
       </ul>
 
