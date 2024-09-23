5bbc2120d0cc71041166e5a2bc4d520c74931c3b
Yohan D Osorio l <yooslzn1487@gmail.com>
2024-09-22 22:05:53 -0500

    Add order component

--- src/pages/order/index.tsx
+++ src/pages/order/index.tsx
@@ -1,11 +1,34 @@
+import { useParams } from 'react-router-dom'
 import Layout from '../../components/layout'
+import OrderCard from '../../components/order-card'
+import { OrderType } from '../../types/types'
 
 function Order() {
- return (
-   <Layout>
-     <div>Your order has been placed!</div>
-   </Layout>
- )
+  const params = useParams()
+  const orderId = params.id
+  const getOrdersJson = localStorage.getItem('orders')
+  const getOrders: OrderType[] = getOrdersJson ? JSON.parse(getOrdersJson) : []
+  
+  const order = getOrders.find(order => order.id === orderId)
+  const orderItems = order?.items || []
+  
+  return (
+    <Layout>
+      <p>Tu Orden</p>
+      <div className='w-[520px] py-2 px-2'>
+        {orderItems.map(item => (
+          <OrderCard
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            imageUrl={item.image}
+            price={item.price}
+          />
+        ))
+        }
+      </div>
+    </Layout>
+  )
 }
 
 export default Order
